1f4979354b8fa91dbfe4ac3f8aadba144ebdd267
Aziosmanoff <nine.aziosmanoff@club.fr>
2016-07-27 16:40:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,2 @@
+var app = angular.module("MyApp", []); //vide pas de route // on dit qu'on va utiliser angular
+console.log("bonjour");
---
+++ controller.js
@@ -0,0 +1,5 @@
+app.controller("AppCtrl", function ($scope, $http){
+
+
+ $scope.showme = false;
+});
